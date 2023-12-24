9907ffcff1e95da74a5acbc937e26abf17ef108d
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-12-24 17:24:27 +0600

    New task addition functionality added

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,11 +1,14 @@
 import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Swal from "sweetalert2";
+import AddNewTaskModal from "../pages/Dashboard/AddNewTaskModal/AddNewTaskModal";
+import { useState } from "react";
 
 const Dashboard = () => {
   const { user, logOut } = useAuth();
   const navigate = useNavigate();
-
+  const [showmodal, setshowmodal] = useState(false);
+  // User logout function
   const handleLogout = () => {
     logOut()
       .then(() => {
@@ -24,15 +27,15 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="flex">
-      <div className="w-full sm:w-60 sm:min-h-screen text-center sm:text-left bg-blue-400 font-bold text-white space-y-4 px-5 py-12">
+    <div className="flex flex-col sm:flex-row">
+      <div className="w-full sm:w-48 md:w-60 sm:min-h-screen text-center sm:text-left bg-blue-400 font-bold text-white space-y-4 px-5 py-12">
         <div className="flex items-center justify-center gap-2">
           <img
             className="w-12 h-12 rounded-full"
-            src={user?.photoUrl}
+            src={user?.photoURL}
             alt="User Photo"
           />
-          <h4 className="font-medium">Welcome {user?.displayname}</h4>
+          <h4 className="font-medium">Welcome {user?.displayName}</h4>
         </div>
         <div className="border"></div>
         <div>
@@ -50,6 +53,42 @@ const Dashboard = () => {
           </button>
         </div>
       </div>
+      <div className="flex-1">
+        <div className="text-right">
+          <button
+            onClick={() => setshowmodal(true)}
+            className="font-semibold m-5 px-5 py-2 bg-black text-white"
+          >
+            Add New Task
+          </button>
+        </div>
+
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 p-5 gap-4">
+          <div className="min-h-96 border rounded-md">
+            <h4 className="bg-blue-700 text-white font-semibold rounded-md text-center p-3">
+              To-Do
+            </h4>
+            <div>All todos here...</div>
+          </div>
+          <div className="min-h-96 border rounded-md">
+            <h4 className="bg-blue-700 text-white font-semibold rounded-md text-center p-3">
+              Ongoing
+            </h4>
+            <div>Ongoing tasks here...</div>
+          </div>
+          <div className="min-h-96 border rounded-md">
+            <h4 className="bg-blue-700 text-white font-semibold rounded-md text-center p-3">
+              Completed
+            </h4>
+            <div>Completed tasks here...</div>
+          </div>
+        </div>
+      </div>
+      {showmodal && (
+        <div className="">
+          <AddNewTaskModal setshowmodal={setshowmodal} />
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/pages/Dashboard/AddNewTaskModal/AddNewTaskModal.jsx
@@ -0,0 +1,185 @@
+import { useForm } from "react-hook-form";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import Swal from "sweetalert2";
+import { MdErrorOutline } from "react-icons/md";
+import PropTypes from "prop-types";
+import useAuth from "../../../hooks/useAuth";
+
+const AddNewTaskModal = ({ setshowmodal }) => {
+  const axiosPublic = useAxiosPublic();
+  const { user } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const task = {
+      ...data,
+      email: user?.email,
+    };
+    axiosPublic.post("/tasks", task).then((res) => {
+      if (res?.data?._id) {
+        Swal.fire({
+          title: "Task Added Successfully!",
+          icon: "success",
+          showConfirmButton: false,
+          timer: 1000,
+        });
+      }
+      setshowmodal(false);
+    });
+  };
+
+  return (
+    <div className="overflow-y-auto overflow-x-hidden fixed z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      <div className="relative p-4 w-full max-w-xl max-h-full">
+        <div className="relative bg-white rounded-lg shadow">
+          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
+            <h3 className="text-lg font-semibold text-gray-900">
+              Add New Task
+            </h3>
+            <button
+              onClick={() => setshowmodal(false)}
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center "
+            >
+              <svg
+                className="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+          </div>
+          <form className="p-5" onSubmit={handleSubmit(onSubmit)} noValidate>
+            <div className="mb-6">
+              <label
+                htmlFor="title"
+                className="block mb-2 text-sm font-medium text-gray-900 "
+              >
+                Title
+              </label>
+              <input
+                type="text"
+                id="title"
+                {...register("title", {
+                  required: "Title is required.",
+                })}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              />
+              <p>
+                {errors.title && (
+                  <span className="text-red-600 text-xs flex items-center gap-0.5">
+                    <MdErrorOutline />
+                    {errors.title?.message}
+                  </span>
+                )}
+              </p>
+            </div>
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="deadline"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Deadline
+                </label>
+                <input
+                  type="date"
+                  id="deadline"
+                  {...register("deadline", {
+                    required: "Deadline is required.",
+                  })}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                />
+                <p>
+                  {errors.deadline && (
+                    <span className="text-red-600 text-xs flex items-center gap-0.5">
+                      <MdErrorOutline />
+                      {errors.deadline?.message}
+                    </span>
+                  )}
+                </p>
+              </div>
+              <div>
+                <label
+                  htmlFor="priority"
+                  className="block mb-2 text-sm font-medium"
+                >
+                  Select Priority
+                </label>
+                <select
+                  id="priority"
+                  {...register("priority", {
+                    required: "Priority is required.",
+                  })}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                >
+                  <option value="">Choose priority</option>
+                  <option value="Low">Low</option>
+                  <option value="Medium">Medium</option>
+                  <option value="High">High</option>
+                </select>
+                <p>
+                  {errors.priority && (
+                    <span className="text-red-600 text-xs flex items-center gap-0.5">
+                      <MdErrorOutline />
+                      {errors.priority?.message}
+                    </span>
+                  )}
+                </p>
+              </div>
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="description"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Description
+              </label>
+              <textarea
+                id="description"
+                rows="3"
+                {...register("description", {
+                  required: "Description is required.",
+                })}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+              ></textarea>
+              <p>
+                {errors.description && (
+                  <span className="text-red-600 text-xs flex items-center gap-0.5">
+                    <MdErrorOutline />
+                    {errors.description?.message}
+                  </span>
+                )}
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              Add New Task
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+AddNewTaskModal.propTypes = {
+  setshowmodal: PropTypes.func,
+};
+
+export default AddNewTaskModal;
